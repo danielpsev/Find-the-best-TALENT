3143a5d25ded46f3a3081125db1031a56490d5c1
Daniel <danielpsev@gmail.com>
2022-12-08 20:01:58 +0200

    .

--- script/main.js
+++ script/main.js
@@ -1,10 +1,14 @@
 let openMobileNavBtn = document.getElementById("openMobileNavBtn");
 let closeMobileNavBtn = document.getElementById("closeMobileNavBtn");
+let mobileNav = document.getElementById("mobileNav");
 openMobileNavBtn.addEventListener("click", function(){
-	let mobileNav = document.getElementById("mobileNav");
 	mobileNav.style.display = "block";
 });
 closeMobileNavBtn.addEventListener("click", function(){
-	let mobileNav = document.getElementById("mobileNav");
 	mobileNav.style.display = "none";
 });
+window.onclick = function(event) {
+    if (event.target.classList.contains('mobile-nav-container')){
+		mobileNav.style.display = "none";
+    }
+}
